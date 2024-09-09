7e752bbf797aa62c96127f44195a9c398b2ab200
arnabadhikaryy <139059216+arnabadhikaryy@users.noreply.github.com>
2024-09-09 11:32:03 +0530

    Update db_cunnect.js

--- backend/Utiles/db_cunnect.js
+++ backend/Utiles/db_cunnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const db_connect =async ()=>{try {
   
-  await mongoose.connect(process.env.mongo_db_url)
+  await mongoose.connect('mongodb+srv://adhikaryarnab977:S73YUVGdyFPsej@arnabadhikary.7edj6ma.mongodb.net/EasySkill')
   .then(() => console.log('Database Connected!'));
 } catch (error) {
   console.log(error);
