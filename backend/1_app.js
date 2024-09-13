ba9c3dc60501fa84d3a75bbff2437068c0402e35
arnabadhikaryy <139059216+arnabadhikaryy@users.noreply.github.com>
2024-09-13 17:31:25 +0530

    Update 1_app.js

--- backend/1_app.js
+++ backend/1_app.js
@@ -6,7 +6,7 @@ import cors from "cors"
 const app = express();
 app.use(cors(
   {
-    origin:'https://66e3be15d9e302958cceaab3--easyskill.netlify.app'
+    origin:'https://easyskill.netlify.app'
 }
 ))//Enable All CORS Requests............
 app.use(express.json());
